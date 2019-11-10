7a80e4e586bb6a9bdd43c1eda65a81ad9a52c44f
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-10 22:04:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,11 +16,11 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contacts_details_hash|
-    binding.pry
+    
 if person == "Freddy Mercury"
 
   contacts_details_hash.each do |attribute, data|
-  
+  binding.pry
 end
 end 
 end 
